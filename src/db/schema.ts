5a68e7a3c7972d58fbbe30706c09c95477206b9d
thoughtake <sought1001@gmail.com>
2025-07-24 10:24:14 +0900

    料理のshemma.tsを作成

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
-import { mysqlTable, serial, int, varchar,} from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, int, varchar, boolean, decimal} from 'drizzle-orm/mysql-core';
 
+//食材
 export const ingredients = mysqlTable('ingredients', {
   id: serial('id').primaryKey(), 
   name: varchar('name', { length: 255 }).notNull(),
@@ -24,3 +25,33 @@ export const ingredientGroupColors = mysqlTable('ingredient_group_colors', {
   ingredientGroupId: int('ingredient_group_id').notNull(),
   colorCode: varchar('color_code', { length: 7 }).notNull(), 
 });
+
+//料理
+export const dishes = mysqlTable('dishes', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  timeMinutes: int('time_minutes'),
+  energyKcal: int('energy_kcal'),
+  isFavorite: boolean('is_favorite').default(false),
+});
+
+export const dishIngredients = mysqlTable("dish_ingredients", {
+  id: serial("id").primaryKey(),
+  dishId: int("dish_id").references(() => dishes.id),
+  ingredientId: int("ingredient_id").references(() => ingredients.id),
+  quantity: decimal("quantity", { precision: 10, scale: 2 }),
+});
+
+export const dishRecipes = mysqlTable("dish_recipes", {
+  id: serial("id").primaryKey(),
+  dishId: int("dish_id").references(() => dishes.id),
+  stepNumber: int("step_number"),
+  description: varchar("description", { length: 1000 }),
+});
+
+export const dishImages = mysqlTable("dish_images", {
+  id: serial("id").primaryKey(),
+  dishId: int("dish_id").references(() => dishes.id),
+  imageUrl: varchar("image_url", { length: 1000 }),
+});
+
