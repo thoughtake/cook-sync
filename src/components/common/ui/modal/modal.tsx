c7ff57fb355af7a6dc77401ef3275fc8de409b09
thoughtake <sought1001@gmail.com>
2025-07-28 16:07:20 +0900

    modalの仕様を変更

--- src/components/common/ui/modal/modal.tsx
+++ src/components/common/ui/modal/modal.tsx
@@ -12,28 +12,46 @@ import {
   DialogPortal,
   DialogTitle,
 } from "@radix-ui/react-dialog";
+import clsx from "clsx";
 
 const Modal = () => {
-  const { isOpen, closeModal, content } = useModal();
+  const { closeModal, contents } = useModal();
 
   return (
-    <Dialog open={isOpen} onOpenChange={(open) => !open && closeModal()}>
-      <DialogPortal>
-        <DialogOverlay className="fixed inset-0 bg-black/50 overflow-y-scroll" />
-        <DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-white p-8 rounded w-940 max-w-[940px] mx-auto outline-primary outline-3 max-h-[90vh] overflow-auto">
-          <div className="text-right">
-            <button onClick={closeModal} className="cursor-pointer">
-              <X />
-            </button>
-          </div>
-          <DialogTitle className="hidden">モーダル</DialogTitle>
-          <DialogDescription className="hidden">
-            モーダルを表示しています。
-          </DialogDescription>
-          <Suspense fallback={<ModalLoading />}>{content}</Suspense>
-        </DialogContent>
-      </DialogPortal>
-    </Dialog>
+    <>
+      {contents.length > 0 &&
+        contents.map((content, index) => (
+          <Dialog
+            key={index}
+            open={true}
+            onOpenChange={(open) => !open && closeModal()}
+          >
+            <DialogPortal>
+              <DialogOverlay
+                className={clsx("fixed inset-0 bg-black/50 overflow-y-scroll")}
+                style={{ zIndex: 50 + index * 2 }}
+              />
+              <DialogContent
+                className={clsx(
+                  "fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-white p-8 rounded w-940 max-w-[940px] mx-auto outline-primary outline-3 max-h-[90vh] overflow-auto"
+                )}
+                style={{ zIndex: 51 + index * 2 }}
+              >
+                <div className="text-right">
+                  <button onClick={closeModal} className="cursor-pointer">
+                    <X />
+                  </button>
+                </div>
+                <DialogTitle className="hidden">モーダル</DialogTitle>
+                <DialogDescription className="hidden">
+                  モーダルを表示しています。
+                </DialogDescription>
+                <Suspense fallback={<ModalLoading />}>{content}</Suspense>
+              </DialogContent>
+            </DialogPortal>
+          </Dialog>
+        ))}
+    </>
   );
 };
 
--- src/context/modal-context.tsx
+++ src/context/modal-context.tsx
@@ -1,39 +1,46 @@
-'use client'
+"use client";
 
 import { createContext, ReactNode, useContext, useState } from "react";
 
 type ContextType = {
   showModal: (content: ReactNode) => void;
   closeModal: () => void;
-  content: ReactNode | null;
-  isOpen: boolean;
-}
+  contents: ReactNode[];
+  // content: ReactNode | null;
+  // isOpen: boolean;
+};
 
 const ModalContext = createContext<ContextType | undefined>(undefined);
 
-export const ModalProvider = ({children}: {children: ReactNode}) => {
-  const [content, setContent] = useState<ReactNode | null>(null);
-  const [isOpen, setIsOpen] = useState(false);
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  // const [content, setContent] = useState<ReactNode | null>(null);
+  // const [isOpen, setIsOpen] = useState(false);
+  const [contents, setContents] = useState<ReactNode[]>([]);
 
   const showModal = (content: ReactNode) => {
-    setContent(content);
-    setIsOpen(true);
+    setContents((prev) => [...prev, content]);
+    // setContent(content);
+    // setIsOpen(true);
   };
 
   const closeModal = () => {
-    setIsOpen(false);
-    setContent(null);
-  }
+    // setIsOpen(false);
+    // setContent(null);
+    setContents((prev) => prev.slice(0, -1));
+  };
 
   return (
-    <ModalContext.Provider value={{showModal, closeModal, content, isOpen}}>
+    <ModalContext.Provider
+      value={{ showModal, closeModal, contents: contents }}
+    >
       {children}
     </ModalContext.Provider>
-  )
+  );
 };
 
 export const useModal = () => {
   const context = useContext(ModalContext);
-  if (!context) throw new Error("useModalがModalProviderの中で使われていません");
+  if (!context)
+    throw new Error("useModalがModalProviderの中で使われていません");
   return context;
-}
+};
